f5db6917f70e5f68a570525c4cf29bf4f0572bd1
danyel93 <danycispra@gmail.com>
2015-05-16 16:40:54 -0500

    cambios de url ....

--- AppFar/urls.py
+++ AppFar/urls.py
@@ -7,10 +7,16 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    # pagina principal
     url(r'^',include('principal.urls')),
+    # url de la aplicacion cliente
     url(r'^cliente/' , include('cliente.urls')),
+    # url de la aplicacion laboratorio
     url(r'^laboratorio/' , include('laboratorio.urls')),
+    # url de la aplicacion medicamento
     url(r'^medicamento/' , include('medicamento.urls')),
+    # url de imagenes
+    url(r'Imagenes/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT, } ),
     
    
 )
